e5654392df35580dd80ed1205fc71e40f4f65a6a
Marina Kukso <marina.kukso@gmail.com>
2014-12-05 14:14:58 -0800

    added tcp-bind

--- server.js
+++ server.js
@@ -1,4 +1,10 @@
 var http = require('http');
 var ecstatic = require('ecstatic');
+var alloc = require('tcp-bind');
+var fd = alloc(80);
+
+process.setgid('ubuntu');
+process.setuid('ubuntu');
+
 var server = http.createServer(ecstatic(__dirname));
-server.listen(5000);
+server.listen({ fd: fd });
